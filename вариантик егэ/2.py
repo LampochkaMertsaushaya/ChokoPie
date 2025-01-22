47a321922b1f3de7c577ab1d226c0b311de9e519
LampochkaMertsaushaya <uliana.sedova@bk.ru>
2025-01-22 17:30:28 +0300

    тестинг

---
+++ вариантик егэ/2.py
@@ -0,0 +1,10 @@
+def f(x, y, z, w):
+    return not((((((w and x)== x)or 1)<= z)or (not x))and y)
+
+
+for x in (0, 1):
+    for y in (0, 1):
+        for z in (0, 1):
+            for w in (0, 1):
+                if f(x, y, z, w) == 0:
+                    print(x, y, z, w, '|', f(x, y, z, w))
